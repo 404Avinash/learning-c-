805f28369c979c6d7aecad15411e287b9b4e88fb
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-19 23:35:40 +0530

    mm

---
+++ types of var/single_char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+//single character is a data type which can store a single character
+char grade = 'A'; //we can also write char grade; and then assign value to it later
+//char grade = 'B';
+char initial ='c'; // this is also a single char variable which can store a single character
+cout<<grade;
+
+
+    return 0;
+}
