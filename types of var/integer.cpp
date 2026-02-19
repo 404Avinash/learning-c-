15c29bb21015e1d51ed8867b2378152d122cde65
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-19 23:23:50 +0530

    mm

--- types of var/integer.cpp
+++ types of var/integer.cpp
@@ -4,6 +4,9 @@ int main ()
 {
 //integer is a data type which can store whole numbers
 int age = 21; //we can also write int age; and then assign value to it later
+int year= 2026;
+int days =7.5; //this will give errror because 7.5 is a floating point number and cannot be stored in an integer variable 
+cout<<days<<endl;
 
 
 
