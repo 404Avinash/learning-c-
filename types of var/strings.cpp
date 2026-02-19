381371a4a004207d49d5460dc3e08aa71ef5aac1
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-20 00:02:56 +0530

    mm

---
+++ types of var/strings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+//string is a data type which can store a sequence of characters
+string name = "batman"; 
+cout<<name;
+string city ="gotham";
+string food = "pizza";
+string day = "Friday";
+
+    return 0;
+}
