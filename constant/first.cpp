a83a519ae495a709d49d96a6a74d37b25f99f285
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-20 00:49:34 +0530

    mm

--- constant/first.cpp
+++ constant/first.cpp
@@ -4,7 +4,7 @@ int main()
 {
 const double PI =3.14; // this is a constant variable because its value cannot be changed
 
-const int LIGH_SPEED= 299792458; //this is also a const variable because its value cannot be changed 
+const int LIGHT_SPEED= 299792458; //this is also a const variable because its value cannot be changed 
 /*double radius =10;
 double circumference=2*PI*radius;
 cout<<"the circumference of the circle with radius 10 is  "<<circumference<<endl;
