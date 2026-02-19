bd17e0aa3e88e0c309b59ed95d794d9e3ab95203
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-20 00:05:00 +0530

    mm

--- types of var basic/strings.cpp
+++ types of var basic/strings.cpp
@@ -4,10 +4,9 @@ int main ()
 {
 //string is a data type which can store a sequence of characters
 string name = "batman"; 
-cout<<name;
 string city ="gotham";
 string food = "pizza";
 string day = "Friday";
-
+cout<<"hello "<<name<<endl;
     return 0;
 }
