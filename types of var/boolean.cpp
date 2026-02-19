a4aa6cb06e511f83a79ed50bd1bcd55719f24fa9
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-19 23:58:27 +0530

    m

---
+++ types of var/boolean.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int mian ()
+{
+//boolean is a data type which can store only two values true or false
+bool power =false; //we can also write bool power; and then assign value to it later
+bool for_sale=true;
+cout<<power<<endl;
+
+
+
+
+    return 0;
+}
