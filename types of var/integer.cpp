700a2f2b1810233e3759611243786429fd32ea4b
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-19 23:20:57 +0530

    m

---
+++ types of var/integer.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+//integer is a data type which can store whole numbers
+int age = 21; //we can also write int age; and then assign value to it later
+
+
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x; // this step is called a declaration 
+x=5; // this step is called assignment 
+cout<<x<<endl; // this step is called usage
+int y = 6;
+cout<<y<<endl;
+
+int sum = x+y;
+cout<<sum<<endl;
+
+    return 0;
+}
