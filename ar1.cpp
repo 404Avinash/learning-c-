32b8edc9da20c4d16fffd7b32464f7d4a6206541
rudrakshtyagi1 <avinash1807007@gmail.com>
2026-02-24 15:26:18 +0530

    up

---
+++ ar1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    cout<<"Enter the array elements";
+    for (i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"The array elements are";
+    for (i=0;i<n;i++)
+    {
+        cout<<a[i];
+    
+    return 0;
+}
