cc12b1a968b11d030ebfe356aca1d44edb24e335
User <user@example.com>
2026-05-26 01:55:38 +0530

    learning

---
+++ tut-1/basics/variableNmaingrules.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//    int x = 4;
+//    cout<<x<<endl;
+//    cout<<++x<<endl;
+//    cout<<x<<endl;
+
+
+//alphabets ab cn cn cn cn crnrr  rr
+// // numbers 1 2 3 4 5 56
+// sp3ecial characters 
+// }
+
+// auto double int not valid as variable name 
+
+
+//invalid basic-hra #M 44 over time team'ss 
+}
