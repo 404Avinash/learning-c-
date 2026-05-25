b556f47cc1bc85ecd6d8ea4d88ef89ea86467738
User <user@example.com>
2026-05-26 02:13:39 +0530

    learning

---
+++ tut-1/basics/sumOfnum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x,y;
+  cout<<"enter a number : ";
+  cin>>x;
+  cout<<"second Number is : ";
+  cin>>y;
+  cout<<x+y;
+  return 0;
+
+
+ }
