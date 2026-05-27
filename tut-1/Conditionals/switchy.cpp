8838fcdf02a3f2645dcf3114ea9302957ec5b5e0
User <user@example.com>
2026-05-27 18:50:59 +0530

    add learn

--- tut-1/Conditionals/switchy.cpp
+++ tut-1/Conditionals/switchy.cpp
@@ -9,9 +9,20 @@ char op;
 cin>>op;
 int n2;
 cin>>n2;
-if(op=='+') cout<<n1+n2;
-if(op=='-') cout<<n1-n2;
-if(op=='*') cout<<n1*n2;
-if(op=='/') cout<<n1/n2;
+switch(op) {
+    case '+':
+        cout<<n1+n2;
+        break;
+    case '-':
+        cout<<n1-n2;
+        break;
+    case '*':
+        cout<<n1*n2;
+        break;
+    case '/':
+        cout<<n1/n2;
+        break;
+    default:
+        cout<<"Invalid operator";
 }
 ///okay check 
